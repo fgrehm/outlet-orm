bfe8adad72f99df7dbbf0a9a3f0bd01dc5be0d0b
Alvaro Carrasco <simplepic@gmail.com>
2008-05-16 05:08:29 +0000

    added some stuff to OutletPDO in order to support mssql
    git-svn-id: svn://knowledgehead.com/outlet/trunk@33 dbdcdb93-603a-0410-b5c8-d6db4ea8acfe

--- classes/outlet/OutletPDO.php
+++ classes/outlet/OutletPDO.php
@@ -1,7 +1,22 @@
 <?php
 
 class OutletPDO extends PDO {
+	private $driver;
 
+	function __construct ($dsn, $user=null, $pass=null) {
+		// store the driver
+		$this->driver = substr($dsn, 0, strpos($dsn, ':'));		
+
+		parent::__construct($dsn, $user, $pass);
+	}
+
+	function lastInsertId($name=null) {
+		if ($this->driver == 'mssql') {
+			return $this->query('SELECT SCOPE_IDENTITY() as id')->fetchColumn('id');
+		} else {
+			return parent::lastInsertId();
+		}
+	}
 }
 
 
