58c30fd341afa3a16b31949a42ea923237ab4f29
Matt Nowack <ihumanable@gmail.com>
2009-08-21 03:06:52 +0000

    Added documentation
    git-svn-id: svn://knowledgehead.com/outlet/outlet-orm/trunk@206 dbdcdb93-603a-0410-b5c8-d6db4ea8acfe

--- classes/outlet/outletgen.php
+++ classes/outlet/outletgen.php
@@ -1,5 +1,9 @@
 <?php
 /**
+ * Command line tool to build proxies, allows byte-code caches to cache the proxies code.
+ * This is an alternative to using Outlet::createProxies()
+ * @see Outlet::createProxies()
+ * @see OutletProxyGenerator::generate() 
  * @package outlet
  */
 set_include_path(dirname(__FILE__).'/..'.PATH_SEPARATOR.get_include_path());
