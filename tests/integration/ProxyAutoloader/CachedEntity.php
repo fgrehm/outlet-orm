3f66cdb324c72db9175375aec11ce6f122b59171
Fabio Rehm <fgrehm@gmail.com>
2009-11-05 15:52:37 -0300

    missing files

---
+++ tests/integration/ProxyAutoloader/CachedEntity.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace outlet\tests\model;
+
+define('PROXY_CACHE_TEST', true);
+
+class CachedEntity_OutletProxy extends CachedEntity implements \outlet\Proxy {}
---
+++ tests/integration/ProxyAutoloader/entities.php
@@ -0,0 +1,12 @@
+<?php
+namespace outlet\tests\model;
+
+class DisabledEntity { }
+
+class OnDemandEntity { }
+
+class CachedEntity { }
+
+class EntityToCache { }
+
+class UnmappedEntity { }
